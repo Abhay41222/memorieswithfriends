ce4639214143e6d7a1e29d4923b184d3af70942d
Krishna Kant <162586429+lazy-universe@users.noreply.github.com>
2025-02-02 16:06:15 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.gallery-image');
-
-    images.forEach(image => {
-        image.addEventListener('click', () => {
-            alert("You clicked on a memory!");
-        });
-    });
-});
